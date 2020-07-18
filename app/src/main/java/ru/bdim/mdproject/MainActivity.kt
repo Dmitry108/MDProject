d3c7c2a9a5e82d53e8f0c58087a5fdb6acac0fa5
Dmitrii Babanov <babanov1984@mail.ru>
2020-07-19 01:39:34 +0300

    materialpalette used

--- app/src/main/java/ru/bdim/mdproject/MainActivity.kt
+++ app/src/main/java/ru/bdim/mdproject/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.DeepPurple_NoActionBar)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
